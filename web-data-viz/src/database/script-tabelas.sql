8ac2d8f72aa7fb1510e7320b418bfc75eecf59dd
Kauã Pedroso <kaua.pedroso@sptech.school>
2026-05-14 18:17:11 -0300

    Integração Web-Data-Viz

--- web-data-viz/src/database/script-tabelas.sql
+++ web-data-viz/src/database/script-tabelas.sql
@@ -1,62 +1,50 @@
-	-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
 -- Você precisa executar os comandos no banco de dados para criar as tabelas,
 -- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
 
 /*
 comandos para mysql server
 */
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
+-- Projeto Individual de PI - OPUS
+CREATE DATABASE opus;
+USE opus;
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+id INT PRIMARY KEY auto_increment,
+nome varchar (45) NOT NULL,
+email varchar (45) NOT NULL,
+senha varchar (45) NOT NULL
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+CREATE TABLE compositor (
+id INT PRIMARY KEY auto_increment,
+nome varchar (60),
+periodo varchar (45)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE estudo (
+id INT PRIMARY KEY auto_increment,
+tecnica varchar (45),
+tempo_minutos INT,
+data_estudo DATETIME DEFAULT current_timestamp,
+fkUsuario INT,
+	CONSTRAINT fkEstudoUsuario	
+		FOREIGN KEY (fkUsuario) 
+			REFERENCES usuario (id),
+fkCompositor INT,
+	CONSTRAINT fkEstudoCompositor
+		FOREIGN KEY (fkCompositor)
+			REFERENCES compositor (id)
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
+INSERT INTO usuario (nome,email,senha) VALUES 
+('Kauã', 'kaua@pedroso','senhaTop@123');
 
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+INSERT INTO compositor (nome,periodo) VALUES 
+('Franz Liszt', 'Romântico'),
+('Chopin','Romântico');
+
+INSERT INTO estudo (tecnica, tempo_minutos, fkUsuario, fkCompositor) VALUES
+('Escalas e Arpejos', 30, 1, 1),
+('Oitavas', 45, 1, 2);
